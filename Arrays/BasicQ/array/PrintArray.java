302ea3b8b0f83b4bd2600389e7df61b536bb44bd
RAUSHANARYA <raushankumarlnct07@gmail.com>
2026-06-24 06:54:41 +0530

    initial commits

---
+++ Arrays/BasicQ/array/PrintArray.java
@@ -0,0 +1,10 @@
+package array;
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Arrays just to check");
+	}
+
+}
